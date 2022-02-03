b18825617bd132dfa227db576367c355dc88353c
Pancho <panchoesteban@gmail.com>
2022-02-03 19:49:32 -0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:preference/prefereces.dart';
 import 'package:preference/screens/screens.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding();
+
+  Preferences.prefs = await SharedPreferences.getInstance();
+  Preferences.prefs.getInt('genero') ?? 1;
+  Preferences.prefs.getString('nombre') ?? 'nombre';
+  Preferences.prefs.getBool('tema') ?? true;
+
+//  int counter = (prefs.getInt('counter') ?? 0) + 1;
+//  print('Pressed $counter times.');
+//  await prefs.setInt('counter', counter);
+
   runApp(const MyApp());
 }
 
